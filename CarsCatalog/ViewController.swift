9919052370ac19c7f147b47b4e93f4c8a9fd0c05
Ershov Ilia <il998x@gmail.com>
2019-10-07 23:23:01 +0300

    Initial Commit

---
+++ CarsCatalog/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  CarsCatalog
+//
+//  Created by Илья Ершов on 07/10/2019.
+//  Copyright © 2019 Ilia Ershov. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
